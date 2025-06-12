5b5b1f527830cfec9ea6cfc2cad7ec25f65cc4a5
Guilherme Muniz <91857590+PatolinoBR007@users.noreply.github.com>
2025-06-12 11:11:41 -0300

    .env

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const {} = require('discord.js')
 const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
